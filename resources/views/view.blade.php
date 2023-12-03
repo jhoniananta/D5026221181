bb81599632977eaefab25ff1705de61ed89590c8
jhoniananta <jhoniananta@gmail.com>
2023-12-03 23:37:59 +0700

    Tugas Pertemuan tanggal 27 November

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -18,34 +18,34 @@
                 <form action="/pegawai/update" method="post" class="form-horizontal " role="form">
                     {{ csrf_field() }}
                     <input type="hidden" name="id" value="{{ $p->pegawai_id }}">
-                    <div class = "form-group">
-                        <label for = "nama" class = "col-sm-2 control-label">Nama</label>
+                    <div class = "form-group row">
+                        <label for = "nama" class = "col-sm-2 control-label align-self-center">Nama:</label>
                         <div class = "col-sm-10">
-                            <input type="text" required="required" name="nama" value="{{ $p->pegawai_nama }}" class="form-control">
+                            <input type="text" required="required" name="nama" value="{{ $p->pegawai_nama }}" class="form-control" readonly>
                         </div>
                     </div>
-                    <div class = "form-group">
-                        <label for = "jabatan" class = "col-sm-2 control-label">Jabatan</label>
+                    <div class = "form-group row">
+                        <label for = "jabatan" class = "col-sm-2 control-label align-self-center">Jabatan:</label>
                         <div class = "col-sm-10">
-                            <input type="text" required="required" name="jabatan" value="{{ $p->pegawai_jabatan }}" class="form-control">
+                            <input type="text" required="required" name="jabatan" value="{{ $p->pegawai_jabatan }}" class="form-control" readonly>
                         </div>
                     </div>
-                    <div class = "form-group">
-                        <label for = "umur" class = "col-sm-2 control-label">Umur</label>
+                    <div class = "form-group row">
+                        <label for = "umur" class = "col-sm-2 control-label align-self-center">Umur:</label>
                         <div class = "col-sm-10">
-                            <input type="number" required="required" name="umur" value="{{ $p->pegawai_umur }}" class="form-control">
+                            <input type="number" required="required" name="umur" value="{{ $p->pegawai_umur }}" class="form-control" readonly>
                         </div>
                     </div>
-                    <div class = "form-group">
-                        <label for = "alamat" class = "col-sm-2 control-label">Alamat</label>
+                    <div class = "form-group row">
+                        <label for = "alamat" class = "col-sm-2 control-label align-self-center">Alamat:</label>
                         <div class = "col-sm-10">
-                            <textarea required="required" name="alamat" class="form-control">{{ $p->pegawai_alamat }}</textarea>
+                            <textarea required="required" name="alamat" class="form-control" readonly>{{ $p->pegawai_alamat }}</textarea>
                         </div>
                     </div>
                 </form>
             </fieldset>
             @endforeach
-            <div class="text-center" style="width:420px">
+            <div class="text-center" >
                 <a href="/pegawai" class="btn btn-primary w-25">Ok</a>
             </div>
         </div>
