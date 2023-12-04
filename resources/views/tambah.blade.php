59754cf87a28bf9b60db3c4a9509a5268e8f5774
jhoniananta <jhoniananta@gmail.com>
2023-12-04 10:26:34 +0700

    Tugas Pertemuan tanggal 27 November

--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -13,29 +13,28 @@
         <div class = "form-group">
             <label for = "nama" class = "col-sm-2 control-label">Nama</label>
             <div class = "col-sm-10">
-                <input required="required" type = "text" class = "form-control" id = "nama"
+                <input required="required" name="nama" type = "text" class = "form-control" id = "nama"
                     placeholder = "Masukan nama ..">
             </div>
         </div>
         <div class = "form-group">
             <label for = "jabatan" class = "col-sm-2 control-label">Jabatan</label>
             <div class = "col-sm-10">
-                <input required="required" type = "text" class = "form-control" id = "jabatan"
+                <input required="required" name="jabatan" type = "text" class = "form-control" id = "jabatan"
                     placeholder = "Masukan jabatan ..">
             </div>
         </div>
         <div class = "form-group">
             <label for = "umur" class = "col-sm-2 control-label">Umur</label>
             <div class = "col-sm-10">
-                <input required="required" type = "number" class = "form-control" id = "umur"
+                <input required="required" name="umur" type = "number" class = "form-control" id = "umur"
                     placeholder = "Masukan umur ..">
             </div>
         </div>
         <div class = "form-group">
             <label for = "alamat" class = "col-sm-2 control-label">Alamat</label>
             <div class = "col-sm-10">
-                <input required="required" type = "text" class = "form-control" id = "alamat"
-                    placeholder = "Masukan alamat .. ">
+                <textarea required="required" name="alamat" type = "text" class = "form-control" id = "alamat" placeholder = "Masukan alamat .. "></textarea>
             </div>
         </div>
         <input type="submit" value="Simpan Data" class="btn btn-primary">
